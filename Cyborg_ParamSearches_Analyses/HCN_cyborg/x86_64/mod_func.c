c6d4ad5a6e8b524e35690f150fd4295d4a34418a
agmccrei <agmccrei@gmail.com>
2017-09-11 15:22:49 -0400

    Commit Cyborg_ParamSearchers_Analyses/HCN_cyborg

---
+++ Cyborg_ParamSearches_Analyses/HCN_cyborg/x86_64/mod_func.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "hocdec.h"
+extern int nrnmpi_myid;
+extern int nrn_nobanner_;
+
+extern void _Ih_reg(void);
+extern void _IKa_reg(void);
+extern void _ingauss_reg(void);
+extern void _Ksoma_reg(void);
+extern void _Nap_reg(void);
+extern void _Nasoma_reg(void);
+
+void modl_reg(){
+  if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+    fprintf(stderr, "Additional mechanisms from files\n");
+
+    fprintf(stderr," Ih.mod");
+    fprintf(stderr," IKa.mod");
+    fprintf(stderr," ingauss.mod");
+    fprintf(stderr," Ksoma.mod");
+    fprintf(stderr," Nap.mod");
+    fprintf(stderr," Nasoma.mod");
+    fprintf(stderr, "\n");
+  }
+  _Ih_reg();
+  _IKa_reg();
+  _ingauss_reg();
+  _Ksoma_reg();
+  _Nap_reg();
+  _Nasoma_reg();
+}
